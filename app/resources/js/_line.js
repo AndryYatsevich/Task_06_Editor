564574ade45bdca5eb19e7ed8af00cd11c0f724a
AndryYatsevich <yatsevichao@gmail.com>
2017-10-19 23:34:13 +0400

    линия двигается, но не совсем правильно

--- app/resources/js/_line.js
+++ app/resources/js/_line.js
@@ -19,24 +19,31 @@ Line.prototype.changePosition = function (x, y) {
 };
 
 Line.prototype.changeCollision = function (x, y) {
-    var minX = Math.min(this.x, this.height) - 3;
-    var maxX = Math.max(this.x, this.height) + 3;
-    var minY = Math.min(this.y, this.width) - 3;
-    var maxY = Math.max(this.y, this.width) + 3;
+    var offset = 10;
+    var minX = Math.min(this.x, this.height) - offset;
+    var maxX = Math.max(this.x, this.height) + offset;
+    var minY = Math.min(this.y, this.width) - offset;
+    var maxY = Math.max(this.y, this.width) + offset;
     var result;
-
+    console.log(minX, maxX, minY, maxY);
     if (x >= minX && x <= maxX && y >= minY && y <= maxY) {
+
         result = ((this.y - y) * (this.height - this.x)) - ((this.x - x) * (this.width - this.y));
         result = result / (Math.sqrt(Math.pow(this.height - this.x, 2) + Math.pow(this.width - this.y, 2)));
 
-        return Math.abs(result) <= 3;
+        return Math.abs(result) <= offset;
     } else {
         return false;
     }
 };
 
-Line.prototype.moveFigure = function (x, y) {
+Line.prototype.moveFigure = function (x, y, offsetX, offsetY) {
+    var x2 = x - this.x;
+    var y2 = y - this.y;
 
     this.x = x;
     this.y = y;
+    this.width = x - this.width;
+    this.height = y - this.height;
+    console.log(offsetX, offsetY);
 };
